5ee952d12efe7bbb35afd418d800a3fceb2cfeb1
Clarissa Mobley <clarissamobley@me.com>
2024-06-27 18:36:16 -0400

    trying to fix bug

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,16 @@
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    port: process.env.DB_PORT,
-  },
+    process.env.DB_URL || process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: process.env.DB_PORT,
+    },
 );
 
 module.exports = sequelize;
