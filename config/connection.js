b033aaee3ffb26310becb98168332b5268db0e14
Clarissa Mobley <clarissamobley@me.com>
2024-06-27 19:06:10 -0400

    changed config

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
 const Sequelize = require('sequelize');
-
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_URL || process.env.DB_NAME,
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
+    process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: 'localhost',
-        dialect: 'postgres',
-        port: 5432,
-    },
-);
+      host: 'localhost',
+      dialect: 'postgres',
+    }
+  );
 
 module.exports = sequelize;
