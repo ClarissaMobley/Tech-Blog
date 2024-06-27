b9af0ff8d3d71658d5762a3076b7876b4a12e49c
Clarissa Mobley <clarissamobley@me.com>
2024-06-27 18:41:36 -0400

    fixing bugs

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: process.env.DB_HOST,
+        host: 'localhost',
         dialect: 'postgres',
-        port: process.env.DB_PORT,
+        port: 5432,
     },
 );
 
